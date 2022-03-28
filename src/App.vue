b6cfc907e3ea03fb1226beef4f716dee5adeb42e
devxiaofan <1353815633@qq.com>
2022-03-28 22:26:33 +0800

    user input

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,32 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+
+import { ref } from 'vue';
+
+const message = ref('HELLO CCYOU');
+
+function reverseMessage() {
+  message.value = message.value.split('').reverse().join('');
+}
+
+function notify() {
+  alert('Navigation was prevented');
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ message }}</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <button @click="reverseMessage">翻转字符串</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="message += '!'">添加！</button>
+
+  <a href="https://github.com" @click.prevent="notify">Go</a>
 </template>
 
 <style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
+button, a {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  margin-bottom: 1em;
 }
 </style>
