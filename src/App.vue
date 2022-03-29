1c49d7cc25ac78cd0428f640c80458983ebe4437
devxiaofan <1353815633@qq.com>
2022-03-29 13:29:37 +0800

    conditionals

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,23 @@
-<script>
+<script setup>
+import { ref } from 'vue'
 
-export default {
-  data() {
-    return {
-      message: 'Options message',
-      isRed: true,
-      color: 'blue',
-    }
-  },
-
-  methods: {
-    toggleRed() {
-      this.isRed = !this.isRed
-    },
-
-    toggleColor() {
-      this.color = this.color === 'blue' ? 'green' : 'blue'
-    },
-  },
-
-}
+const isShow = ref(true)
+const list = ref([1, 2, 3, 4])
 
 </script>
 
 <template>
-  <p>
-    <span :title="message">
-      Hover me to show my title
-    </span>
-  </p>
+  <button @click="isShow = !isShow">Toggle show list</button>
+  <button @click="list.push(list.length+1)">Push Number</button>
+  <button @click="list.pop()">Pop Number</button>
+  <button @click="list.reverse()">Reverse Number</button>
 
-  <p :class="{red: isRed}" @click="toggleRed">
-    This red color? Click me
-  </p>
+  <ul v-if="isShow && list.length">
+    <li v-for="item of list">{{ item }}</li>
+  </ul>
 
-  <p :style="{color}" @click="toggleColor">
-    What color me now? Click to toggle
-  </p>
+  <p v-else-if="list.length">List is not empty, but had hidden</p>
+  <p v-else>List is Empty</p>
 
 </template>
 
