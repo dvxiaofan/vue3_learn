bf1f931fe3fdbf1dcc233a7ff5cdeb8e495abddf
devxiaofan <1353815633@qq.com>
2022-03-30 17:21:59 +0800

    treeView

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <script setup>
-import DemoGrid from '@/components/Grid.vue'
+import TreeItem from '@/components/TreeItem.vue'
 import { ref } from 'vue'
 
-const searchQuery = ref('')
-const gridColumns = ['name', 'power']
-const gridData = [
-  {name: 'CC You', power: Infinity},
-  {name: 'XF fan', power: 9000},
-  {name: 'dd kao', power: 7000},
-  {name: 'rr you', power: 8000},
-]
+const treeData = ref({
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'world' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'world' }]
+        },
+        { name: 'hello' },
+        { name: 'world' },
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'world' }]
+        }
+      ]
+    }
+  ]
+})
 </script>
 
 <template>
-  <form id="search">
-    Search <input name="query" v-model="searchQuery">
-  </form>
 
-  <DemoGrid :data="gridData" :columns="gridColumns"
-            :filter-key="searchQuery"></DemoGrid>
+  <ul>
+    <TreeItem class="item" :model="treeData"></TreeItem>
+  </ul>
 </template>
+
+<style>
+
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
