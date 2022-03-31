434327e75f13910df593f21ad036bf24e533c73d
devxiaofan <1353815633@qq.com>
2022-03-31 10:00:05 +0800

    svgGraph

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,101 @@
 <script setup>
-import TreeItem from '@/components/TreeItem.vue'
-import { ref } from 'vue'
+import PolyGraph from '@/components/PolyGraph.vue'
+import { ref, reactive } from 'vue'
 
-const treeData = ref({
-  name: 'My Tree',
-  children: [
-    { name: 'hello' },
-    { name: 'world' },
-    {
-      name: 'child folder',
-      children: [
-        {
-          name: 'child folder',
-          children: [{ name: 'hello' }, { name: 'world' }]
-        },
-        { name: 'hello' },
-        { name: 'world' },
-        {
-          name: 'child folder',
-          children: [{ name: 'hello' }, { name: 'world' }]
-        }
-      ]
-    }
-  ]
-})
+const newLabel = ref('')
+const stats = reactive([
+  {label: 'A', value: 100},
+  {label: 'B', value: 100},
+  {label: 'C', value: 100},
+  {label: 'D', value: 100},
+  {label: 'E', value: 100},
+  {label: 'F', value: 100},
+])
+
+const add = (e) => {
+  e.preventDefault()
+  if (!newLabel.value) return
+  stats.push({
+    label: newLabel.value,
+    value: 100,
+  })
+  newLabel.value = ''
+}
+
+const remove = (stat) => {
+  if (stats.length > 3) {
+    stats.splice(stats.indexOf(stat), 1)
+  } else {
+    alert("Can't delete more")
+  }
+}
 </script>
 
 <template>
+  <svg width="200" height="200">
+    <PolyGraph :stats="stats"></PolyGraph>
+  </svg>
 
-  <ul>
-    <TreeItem class="item" :model="treeData"></TreeItem>
-  </ul>
+  <!-- controls -->
+  <div v-for="stat in stats">
+    <label>{{ stat.label }}</label>
+    <input type="range" v-model="stat.value" min="0" max="100">
+    <button @click="remove(stat)" class="remove">X</button>
+  </div>
+
+  <form id="add">
+    <input name="newLabel" v-model="newLabel">
+    <button @click="add">Add a Stat</button>
+  </form>
+
+  <pre id="raw">{{ stats }}</pre>
+
 </template>
 
 <style>
-
-.item {
-  cursor: pointer;
-  line-height: 1.5;
+polygon {
+  fill: #42b983;
+  opacity: 0.75;
 }
-.bold {
-  font-weight: bold;
+
+circle {
+  fill: transparent;
+  stroke: #999;
+}
+
+text {
+  font-size: 10px;
+  fill: #666;
+}
+
+label {
+  display: inline-block;
+  margin-left: 10px;
+  width: 20px;
+}
+
+#raw {
+  position: absolute;
+  top: 0;
+  left: 300px;
 }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
