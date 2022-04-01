ecca7039ce00937d6548e3cf8dc6d96cc4e33b56
devxiaofan <1353815633@qq.com>
2022-04-01 11:31:02 +0800

    duration

--- src/App.vue
+++ src/App.vue
@@ -1,176 +1,48 @@
 <script setup>
-import { ref, computed, watchEffect } from 'vue'
+import { onUnmounted, ref } from 'vue';
 
-const STORAGE_KEY = 'vue-todomvc'
+const duration = ref(15 * 1000);
+const elapsed = ref(0);
 
-const filters = {
-  all: (todos) => todos,
-  active: (todos) => todos.filter((todo) => !todo.completed),
-  completed: (todos) => todos.filter((todo) => todo.completed),
+let lastTime = performance.now();
+let handle;
+const update = () => {
+  const time = performance.now();
+  elapsed.value += Math.min(time - lastTime, duration.value - elapsed.value);
+  lastTime = time;
+  handle = requestAnimationFrame(update);
 }
 
-// state
-const todos = ref(JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'))
-const visibility = ref('all')
-const editedTodo = ref()
+update()
 
-// derived state
-const filteredTodos = computed(() => filters[visibility.value](todos.value))
-const remaining = computed(() => filters.active(todos.value).length)
-
-// handle routing
-window.addEventListener('hashchange', onHashChange)
-onHashChange()
-
-// persist state
-watchEffect(() => {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(todos.value))
+onUnmounted(() => {
+  cancelAnimationFrame(handle)
 })
-
-function toggleAll(e) {
-  todos.value.forEach((todo) => (todo.completed = e.target.checked))
-}
-
-function addTodo(e) {
-  const value = e.target.value.trim()
-  if (value) {
-    todos.value.push({
-      id: Date.now(),
-      title: value,
-      completed: false,
-    })
-    e.target.value = ''
-  }
-}
-
-function removeTodo(todo) {
-  todos.value.splice(todos.value.indexOf(todo), 1)
-}
-
-let beforeEditCache = ''
-
-function editTodo(todo) {
-  beforeEditCache = todo.title
-  editedTodo.value = todo
-}
-
-function cancelEdit(todo) {
-  editedTodo.value = null
-  todo.title = beforeEditCache
-}
-
-function doneEdit(todo) {
-  if (editedTodo.value) {
-    editedTodo.value = null
-    todo.title = todo.title.trim()
-    if (!todo.title) removeTodo(todo)
-  }
-}
-
-function removeCompleted() {
-  todos.value = filters.active(todos.value)
-}
-
-function onHashChange() {
-  const route = window.location.hash.replace(/#\/?/, '')
-  if (filters[route]) {
-    visibility.value = route
-  } else {
-    window.location.hash = ''
-    visibility.value = 'all'
-  }
-}
 </script>
 
 <template>
-  <section class="todoapp">
-    <header class="header">
-      <h1>todos</h1>
-      <input
-          class="new-todo"
-          autofocus
-          placeholder="What needs to be done?"
-          @keyup.enter="addTodo"
-      >
-    </header>
-    <section class="main" v-show="todos.length">
-      <input
-          id="toggle-all"
-          class="toggle-all"
-          type="checkbox"
-          :checked="remaining === 0"
-          @change="toggleAll"
-      >
-      <label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        <li
-            v-for="todo in filteredTodos"
-            class="todo"
-            :key="todo.id"
-            :class="{ completed: todo.completed, editing: todo === editedTodo }"
-        >
-          <div class="view">
-            <input class="toggle" type="checkbox" v-model="todo.completed">
-            <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
-            <button class="destroy" @click="removeTodo(todo)"></button>
-          </div>
-          <input
-              v-if="todo === editedTodo"
-              class="edit"
-              type="text"
-              v-model="todo.title"
-              @vnode-mounted="({ el }) => el.focus()"
-              @blur="doneEdit(todo)"
-              @keyup.enter="doneEdit(todo)"
-              @keyup.escape="cancelEdit(todo)"
-          >
-        </li>
-      </ul>
-    </section>
-    <footer class="footer" v-show="todos.length">
-      <span class="todo-count">
-        <strong>{{ remaining }}</strong>
-        <span>{{ remaining === 1 ? 'item' : 'items' }} left</span>
-      </span>
-      <ul class="filters">
-        <li>
-          <a href="#/all" :class="{ selected: visibility === 'all' }">All</a>
-        </li>
-        <li>
-          <a href="#/active" :class="{ selected: visibility === 'active' }">Active</a>
-        </li>
-        <li>
-          <a href="#/completed"
-             :class="{ selected: visibility === 'completed' }">Completed</a>
-        </li>
-      </ul>
-      <button class="clear-completed" @click="removeCompleted"
-              v-show="todos.length > remaining">
-        Clear completed
-      </button>
-    </footer>
-  </section>
+  <label>Elapsed Time:
+    <progress :value="elapsed / duration"></progress>
+  </label>
+
+  <div>{{ (elapsed / 1000).toFixed(1) }} s</div>
+
+  <div>
+    Duration: <input type="range" v-model="duration" min="1" max="30000">
+    {{ (duration / 1000).toFixed(1) }} s
+  </div>
+
+  <button @click="elapsed = 0">Reset</button>
 </template>
 
 <style>
-@import './index.css';
+.elapsed-container {
+  width: 300px;
+}
+
+.elapsed-bar {
+  background-color: red;
+  height: 10px;
+}
+
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
